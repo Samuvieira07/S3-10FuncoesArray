fc8c20bac96d9d487e67d30fceec799d556f4913
Samuel Vieira <samuveira07@gmail.com>
2022-04-12 20:12:35 -0300

    atividade finalizada

---
+++ entrega.js
@@ -0,0 +1,19 @@
+function podio(firstPlace, seconPlace, lastPlace) {
+  let posicoes = [firstPlace, seconPlace, lastPlace];
+
+  if (posicoes[2] == "Daniel") {
+    return console.log(
+      `Primeiro Lugar:${posicoes[0]}, Segundo Lugar:${posicoes[2]}, Terceiro Lugar:${posicoes[1]}`
+    );
+  } else if (posicoes[2] == "Daniel") {
+    return console.log(
+      `Primeiro Lugar:${posicoes[1]}, Segundo Lugar:${posicoes[0]}, Terceiro Lugar:${posicoes[2]}`
+    );
+  } else {
+    return console.log(
+      `Primeiro Lugar:${posicoes[0]}, Segundo Lugar:${posicoes[1]}, Terceiro Lugar:${posicoes[2]}`
+    );
+  }
+}
+
+podio("Rafael", "Daniel", "Manoel");
